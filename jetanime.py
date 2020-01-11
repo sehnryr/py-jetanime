24c789786418fd544cb7c258fba2a01254ff320d
Sehnryr <sehnryr@gmail.com>
2020-01-11 19:00:52 +0100

    getEpisodeNum added

--- jetanime.py
+++ jetanime.py
@@ -6,69 +6,6 @@ from urllib.parse import urljoin, urlparse
 global jetanime_url
 jetanime_url = 'https://www.jetanime.cc'
 
-def getAnimeList():
-    session = HTMLSession()
-    r = session.get(jetanime_url)
-    soup = r.html.find('body', first=True)
-    options = soup.find('option')
-
-    animes = []
-    for option in options:
-        name = unidecode(option.text)
-        url = option.attrs['value']
-        animes.append((name, url))
-
-    return dict(animes)
-
-def getAnimeName(url):
-    url = urlparse(url)
-    if str(url.path).startswith('/anime') == True:
-        _dict = getAnimeList()
-        inverted_dict = dict()
-        for key, value in _dict.items():
-            inverted_dict.setdefault(value, list()).append(key)
-
-        return inverted_dict[url.path]
-    else:
-        session = HTMLSession()
-        r = session.get(url.geturl())
-        soup = r.html.find('body', first=True)
-        ul = soup.find('ul')
-        for u in ul:
-            for c in dict(u.attrs):
-                if c == 'class':
-                    if list(dict(u.attrs)[c])[0] == 'breadcrumb':
-                        temp = u.find('li')
-                        return unidecode(temp[1].text)
-
-def getGounLimitedUrl(url):
-    session = HTMLSession()
-    r = session.get(url)
-    r.html.render()
-    soup = r.html.find('body', first=True)
-    a = soup.find('p')
-
-    def Find(string): 
-        url = re.findall(r'http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string) 
-        return url 
-
-    for i in a:
-        if 'iframe' in str(i.html):
-            link = str(i.html)
-            result = Find(link)
-    
-    url = result[0]
-    session = HTMLSession()
-    r = session.get(url)
-    soup = r.html.find('script')
-    for i in soup:
-        if 'mp4' in str(i.html):
-            sauce = str(i.html)
-            break
-    sauce_string = sauce.split('|')
-    link = f"https://{sauce_string[-5]}.gounlimited.to/{sauce_string[-6]}/v.mp4"
-    return link
-
 class vid:
     def __init__(self, url):
         session = HTMLSession()
@@ -111,3 +48,78 @@ class vid:
     def getFilmList(self):
         if len(self.episodes) > 1:
             return self.episodes[1]
+
+def getAnimeList():
+    session = HTMLSession()
+    r = session.get(jetanime_url)
+    soup = r.html.find('body', first=True)
+    options = soup.find('option')
+
+    animes = []
+    for option in options:
+        name = unidecode(option.text)
+        url = option.attrs['value']
+        animes.append((name, url))
+
+    return dict(animes)
+
+def getAnimeName(url):
+    url = urlparse(url)
+    if str(url.path).startswith('/anime') == True:
+        _dict = getAnimeList()
+        inverted_dict = dict()
+        for key, value in _dict.items():
+            inverted_dict.setdefault(value, list()).append(key)
+
+        return inverted_dict[url.path]
+    else:
+        session = HTMLSession()
+        r = session.get(url.geturl())
+        soup = r.html.find('body', first=True)
+        ul = soup.find('ul')
+        for u in ul:
+            for c in dict(u.attrs):
+                if c == 'class':
+                    if list(dict(u.attrs)[c])[0] == 'breadcrumb':
+                        temp = u.find('li')
+                        return unidecode(temp[1].text)
+
+def getEpisodeNum(url):
+    url = urlparse(url)
+    v = vid(url.geturl())
+    _dict = v.getEpisodeList()
+    inverted_dict = dict()
+    for key, value in _dict.items():
+        inverted_dict.setdefault(value, list()).append(key)
+    number = str()
+    for num in inverted_dict[url.path]:
+        number += f"{num}-"
+    return number[:-1]
+
+def getVideoUrl(url):
+    session = HTMLSession()
+    r = session.get(url)
+    r.html.render()
+    soup = r.html.find('body', first=True)
+    a = soup.find('p')
+
+    def Find(string): 
+        url = re.findall(r'http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string) 
+        return url 
+
+    for i in a:
+        if 'iframe' in str(i.html):
+            link = str(i.html)
+            result = Find(link)
+    
+    url = result[0]
+    session = HTMLSession()
+    r = session.get(url)
+    soup = r.html.find('script')
+    for i in soup:
+        if 'mp4' in str(i.html):
+            sauce = str(i.html)
+            break
+    sauce_string = sauce.split('|')
+    link = f"https://{sauce_string[-5]}.gounlimited.to/{sauce_string[-6]}/v.mp4"
+    return link
