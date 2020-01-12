a10eaebb954ac4823f139d8a91bff806d585a0a1
Sehnryr <sehnryr@gmail.com>
2020-01-12 18:29:11 +0100

    efficiency of vidincreased
    also tried to optimise getAnimeList but not as efficient a the current one

--- jetanime.py
+++ jetanime.py
@@ -8,90 +8,59 @@ jetanime_url = 'https://www.jetanime.cc'
 
 class vid:
     def __init__(self, url):
+
+        def vosta(arg):
+            lang = 'VOSTFR'
+            if arg.find('VOSTA') >= 0:
+                arg = arg.replace(' (VOSTA)', '')
+                lang = 'VOSTA'
+            return arg, lang
+
         url = urljoin('https://www.jetanime.cc', url)
         session = HTMLSession()
         r = session.get(url)
-        soup = r.html.find('body', first=True)
-        lines = soup.find('div')
-        self.list = list()
-        self.films = list()
-        self.episodes = list()
-        for line in lines:
-            attributes = line.attrs
-            try:
-                if attributes['role'] == 'tablist':
-                    cat = line.find('div')
-                    for c in cat:
-                        attributes = c.attrs
-                        try:
-                            if attributes['class'] == ('panel', 'panel-default'):
-                                sec = c.find('div')
-                                for s in sec:
-                                    _s = s.attrs
-                                    try:
-                                        if str(_s['id']).find('sec-') >= 0:
-                                            soup = s.find('a')
-                                            temp_film = list()
-                                            temp_episode = list()
-                                            for sauce in soup:
-                                                num = unidecode(sauce.text)
-                                                link = sauce.attrs['href']
-                                                lang = 'VOSTFR'
-                                                if num.find('(VOSTA)') >= 0:
-                                                    num = num.replace(' (VOSTA)', '')
-                                                    lang = 'VOSTA'
-                                                for s in sec:
-                                                    _s = s.attrs
-                                                    try:
-                                                        if str(_s['class']).find('panel-title') >= 0:
-                                                            search = unidecode(s.text)
-                                                            if search.find('Film') >= 0:
-                                                                if num.find(':') >= 0:
-                                                                    _list = num.split(': ', 1)
-                                                                    num = _list[0].replace('Film ', '')
-                                                                    name = _list[1]
-                                                                    temp_film.append((num, [name, link, lang]))
-                                                                else:
-                                                                    num = num.replace('Film ', '')
-                                                                    temp_film.append((num, [link, lang]))
-                                                                
-                                                            if search.find('Episode') >= 0:
-                                                                num = num.replace('Episode ', '')
-                                                                if num.find('-') >= 0:
-                                                                    _list = num.split('-')
-                                                                    _list.reverse()
-                                                                    for num in _list:
-                                                                        temp_episode.append((num, [link, lang]))
-                                                                else:
-                                                                    temp_episode.append((num, [link, lang]))
-                                                    except KeyError:
-                                                        pass
-                                            if temp_film:
-                                                self.films.append(temp_film)
-                                            if temp_episode:
-                                                self.episodes.append(temp_episode)
-                                    except KeyError:
-                                        pass
-                        except KeyError:
-                            pass
-            except KeyError:
-                pass
-
-        for idx, film in enumerate(self.films):
-            film.reverse()
-            self.films[idx] = dict(film)
-        try:
-            self.films = self.films[0]
-        except IndexError:
+        soup = r.html.xpath("//div[@role='tablist']")
+        sauce = soup[0].xpath("//div[@class='panel panel-default']")
+        for s in sauce:
+            category = unidecode(s.text).split(':\n', 1)[0]
+            vegetables = s.find('a')
+            temp = list()
             self.films = None
-
-        for idx, episode in enumerate(self.episodes):
-            episode.reverse()
-            self.episodes[idx] = dict(episode)
-        try:
-            self.episodes = self.episodes[0]
-        except IndexError:
+            if category.lower() == 'films':
+                for vegetable in vegetables:
+                    link = vegetable.attrs['href']
+                    leaf = unidecode(vegetable.text)
+                    foliage = leaf.split(': ', 1)
+                    if len(foliage) > 1:
+                        number = foliage[0].replace('Film ', '')
+                        name = foliage[1]
+                    else:
+                        name = foliage[0]
+                        number = name.replace('Film ', '')
+                    spices = name.split(' ')
+                    for idx, spice in enumerate(spices):
+                        spices[idx] = spice.capitalize()
+                    name = ' '.join(spices)
+                    fruits = vosta(name)
+                    number = fruits[0]
+                    lang = fruits[1]
+                    temp.append((number, [name, link, lang]))
+                temp.reverse()
+                temp = dict(temp)
+                self.films = temp
             self.episodes = None
+            if category.lower() == 'episodes':
+                for vegetable in vegetables:
+                    link = vegetable.attrs['href']
+                    number = unidecode(vegetable.text).replace('Episode ', '')
+                    fruits = vosta(number)
+                    number = fruits[0]
+                    lang = fruits[1]
+                    temp.append((number, [link, lang]))
+                temp.reverse()
+                temp = dict(temp)
+                self.episodes = temp
+            self.list = [self.films, self.episodes]
 
 def getAnimeList():
     session = HTMLSession()
@@ -120,6 +89,28 @@ def getAnimeList():
         animes.append((name, url))
 
     return dict(animes)
+
+# def getAnimeList1():
+
+#     def deCFEmail(fp):
+#         r = int(fp[:2],16)
+#         email = ''.join([chr(int(fp[i:i+2], 16) ^ r) for i in range(2, len(fp), 2)])
+#         return email
+
+#     session = HTMLSession()
+#     r = session.get(jetanime_url)
+#     soup = r.html.xpath("//select[@id='liste_animes']")
+#     sauce = soup[0].xpath("//option")
+#     animes = list()
+#     for anime in sauce:
+#         link = anime.attrs['value']
+#         email = anime.xpath("//template")
+#         name = unidecode(anime.text)
+#         if email:
+#             email = unidecode(deCFEmail(email[0].attrs['data-cfemail']))
+#             name = name.replace('[email protected]', email).replace('\n', ' ')
+#         animes.append((name, link))
+#     return dict(animes)
 
 def getAnimeName(url):
     url = urlparse(url)
@@ -192,3 +183,17 @@ def getVideoUrl(url):
         except AttributeError:
             url = 'NULL'
             return url
+
+class getLen:
+    def __init__(self, url):
+        v = vid(url)
+        eps = v.episodes
+        if eps is not None:
+            self.episodesLen = len(eps)
+        else:
+            self.episodesLen = 0
+        fms = v.films
+        if fms is not None:
+            self.filmsLen = len(fms)
+        else:
+            self.filmsLen = 0
